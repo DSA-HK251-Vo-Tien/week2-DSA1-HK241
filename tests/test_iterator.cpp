e66606a0f8e58f0aed32f1f923652f6de4e89345
Hen Pham <hen.pham2410968@hcmut.edu.vn>
2025-07-31 09:09:26 +0700

    Add test case 32-DSA1-HEN only

--- tests/test_iterator.cpp
+++ tests/test_iterator.cpp
@@ -33,5 +33,42 @@ TEST_SUITE("DoublyLinkedList Iterator")
         }
     }
 
-    // TODO add test case
+    TEST_CASE("Reverse iteration using Iterator")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(10);
+        list.insertAtTail(20);
+        list.insertAtTail(30);
+
+        int expected[] = {10, 20, 30}; // it should be {10, 20, 30}
+
+        SUBCASE("Reverse iteration using Iterator")
+        {
+            int idx = 2; // Start from last element
+
+            // list.end() is not correct
+            for (auto it = list.end(); it != list.begin(); )
+            {
+                --it; // Move to previous element
+                CHECK(*it == expected[idx--]);
+            }
+            CHECK(idx == -1); // All elements should be checked
+        }
+    }
+
+    TEST_CASE("Iterator equality and inequality")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(1);
+        list.insertAtTail(2);
+
+        auto it1 = list.begin();
+        auto it2 = list.begin();
+        auto it3 = list.end();
+
+        CHECK(it1 == it2); // Same position
+        CHECK(it1 != it3); // Different positions
+    }
+
+    //TODO add test case
 }
