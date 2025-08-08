8399c97c4f30304b5f6718d268b4756faafd62d9
pak <phananhkhoa0123@gmail.com>
2025-08-08 13:07:09 +0000

    38-DSA1-Khoa

--- src/DoublyLinkedList.h
+++ src/DoublyLinkedList.h
@@ -17,8 +17,8 @@ private:
         Node(const T &val, Node *prev = nullptr, Node *next = nullptr) : data(val), prev(prev), next(next) {}
     };
 
-    Node *head; // Dummy head
-    Node *tail; // Dummy tail
+    Node *head;
+    Node *tail;
     int length;
 
 public:
@@ -40,46 +40,33 @@ public:
     {
     private:
         Node *current;
+        const DoublyLinkedList<T> *list;
 
     public:
-        Iterator(Node *node) : current(node) {}
+        Iterator(Node *node, const DoublyLinkedList<T> *list) : current(node), list(list) {}
 
-        T &operator*() const
-        {
+        // TODO implement Iterator
+        T &operator*() {
             return current->data;
         }
-
-        Iterator &operator++()
-        {
+        Iterator &operator++() {
             current = current->next;
             return *this;
         }
-
-        Iterator &operator--()
-        {
-            current = current->prev;
-            return *this;
-        }
-
-        bool operator==(const Iterator &other) const
-        {
-            return current == other.current;
-        }
-
-        bool operator!=(const Iterator &other) const
-        {
+        bool operator!=(const Iterator &other) const {
             return current != other.current;
         }
     };
 
     Iterator begin() const
     {
-        return Iterator(head->next);
+        return Iterator(head->next, this);
     }
 
     Iterator end() const
     {
-        return Iterator(tail);
+        return Iterator(tail, this);
     }
 };
+
 #endif // __DOUBLY_LINKED_LIST_H__
