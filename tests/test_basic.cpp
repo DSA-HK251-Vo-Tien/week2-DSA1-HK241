65cffd88d4f5781a96225d307e2c4a63d7e34ad1
Ivy <nguyentrmy2@gmail.com>
2025-08-24 18:54:10 +0700

    code done

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -83,3 +83,115 @@ TEST_SUITE("DoublyLinkedList<Point> Operations")
 }
 
 // TODO add test case
+TEST_SUITE("DoublyLinkedList Additional Tests")
+{
+    TEST_CASE("Insert at arbitrary positions")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(1);
+        list.insertAtTail(3);
+        list.insertAt(1, 2); // [1,2,3]
+        CHECK(list.size() == 3);
+        CHECK(list.get(0) == 1);
+        CHECK(list.get(1) == 2);
+        CHECK(list.get(2) == 3);
+    }
+
+    TEST_CASE("Delete head element")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(10);
+        list.insertAtTail(20);
+        list.deleteAt(0); // remove 10
+        CHECK(list.size() == 1);
+        CHECK(list.get(0) == 20);
+    }
+
+    TEST_CASE("Delete tail element")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(10);
+        list.insertAtTail(20);
+        list.deleteAt(1); // remove 20
+        CHECK(list.size() == 1);
+        CHECK(list.get(0) == 10);
+    }
+
+    TEST_CASE("IndexOf and contains")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(5);
+        list.insertAtTail(6);
+        list.insertAtTail(7);
+        CHECK(list.indexOf(6) == 1);
+        CHECK(list.contains(7));
+        CHECK_FALSE(list.contains(10));
+    }
+
+    TEST_CASE("Reverse empty list")
+    {
+        DoublyLinkedList<int> list;
+        list.reverse();
+        CHECK(list.size() == 0);
+    }
+
+    TEST_CASE("Reverse single element list")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtHead(42);
+        list.reverse();
+        CHECK(list.size() == 1);
+        CHECK(list.get(0) == 42);
+    }
+
+    TEST_CASE("Iterator traverses list")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(1);
+        list.insertAtTail(2);
+        list.insertAtTail(3);
+
+        int values[3];
+        int idx = 0;
+
+        for (auto it = list.begin(); it != list.end(); ++it) {
+            values[idx++] = *it;
+        }
+
+        CHECK(idx == 3);
+        CHECK(values[0] == 1);
+        CHECK(values[1] == 2);
+        CHECK(values[2] == 3);
+    }
+
+
+    TEST_CASE("Iterator allows modification")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(1);
+        list.insertAtTail(2);
+        for (auto it = list.begin(); it != list.end(); ++it) {
+            *it = *it + 10;
+        }
+        CHECK(list.get(0) == 11);
+        CHECK(list.get(1) == 12);
+    }
+
+    TEST_CASE("ToString with default convert2str")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(1);
+        list.insertAtTail(2);
+        std::string s = list.toString();
+        CHECK(s == "[1, 2]");
+    }
+
+    TEST_CASE("Exception when index out of range")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(99);
+        CHECK_THROWS(list.get(5));
+        CHECK_THROWS(list.deleteAt(2));
+        CHECK_THROWS(list.insertAt(3, 100));
+    }
+}
--- tests/test_iterator.cpp
+++ tests/test_iterator.cpp
@@ -34,4 +34,116 @@ TEST_SUITE("DoublyLinkedList Iterator")
     }
 
     // TODO add test case
+    TEST_CASE("Empty list begin == end")
+    {
+        DoublyLinkedList<int> list;
+        CHECK(list.begin() == list.end());
+    }
+
+    TEST_CASE("Single element list iteration")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(42);
+        auto it = list.begin();
+        CHECK(it != list.end());
+        CHECK(*it == 42);
+        ++it;
+        CHECK(it == list.end());
+    }
+
+    TEST_CASE("Iterator prefix increment")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(1);
+        list.insertAtTail(2);
+        auto it = list.begin();
+        ++it;
+        CHECK(*it == 2);
+    }
+
+    TEST_CASE("Iterator postfix increment")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(5);
+        list.insertAtTail(10);
+        auto it = list.begin();
+        auto old = it++;
+        CHECK(*old == 5);
+        CHECK(*it == 10);
+    }
+
+    TEST_CASE("Iterator prefix decrement")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(7);
+        list.insertAtTail(8);
+        auto it = list.end();
+        --it;
+        CHECK(*it == 8);
+    }
+
+    TEST_CASE("Iterator postfix decrement")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(11);
+        list.insertAtTail(22);
+        list.insertAtTail(33);
+        auto it = list.end();
+        auto old = it--;
+        CHECK(old == list.end()); // old giữ vị trí end
+        --old; // lùi lại
+        CHECK(*old == 33);
+    }
+
+    TEST_CASE("Iterators from copy are equal")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(99);
+        auto it1 = list.begin();
+        auto it2 = it1;
+        CHECK(it1 == it2);
+        CHECK(*it2 == 99);
+    }
+
+    TEST_CASE("Iterating backwards from end to begin")
+    {
+        DoublyLinkedList<int> list;
+        for (int i = 1; i <= 3; i++) list.insertAtTail(i);
+        int expected[] = {3, 2, 1};
+        int idx = 0;
+        for (auto it = --list.end(); ; --it)
+        {
+            CHECK(*it == expected[idx++]);
+            if (it == list.begin()) break;
+        }
+        CHECK(idx == 3);
+    }
+
+    TEST_CASE("Range-based for with empty list")
+    {
+        DoublyLinkedList<int> list;
+        int cnt = 0;
+        for (int x : list) {
+            (void)x;
+            cnt++;
+        }
+        CHECK(cnt == 0);
+    }
+
+    TEST_CASE("Modify elements via iterator")
+    {
+        DoublyLinkedList<int> list;
+        list.insertAtTail(1);
+        list.insertAtTail(2);
+        for (auto it = list.begin(); it != list.end(); ++it)
+        {
+            *it = *it * 10;
+        }
+        int expected[] = {10, 20};
+        int idx = 0;
+        for (auto x : list)
+        {
+            CHECK(x == expected[idx++]);
+        }
+    }
 }
