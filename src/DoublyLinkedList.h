d98957954ed2294a18a1ef8f638ed89f9e8055a4
Thinh <tthinh2006@gmail.com>
2025-09-08 21:34:15 +0700

    code done

--- src/DoublyLinkedList.h
+++ src/DoublyLinkedList.h
@@ -22,10 +22,16 @@ private:
     int length=0;
 
 public:
-    DoublyLinkedList();
-    ~DoublyLinkedList();
+    DoublyLinkedList(){
 
-    void insertAtHead(T data);
+    }
+    ~DoublyLinkedList(){
+
+    }
+
+    void insertAtHead(T data){
+        
+    }
     void insertAtTail(T data);
     void insertAt(int index, T data);
     void deleteAt(int index);
